f52ec0c6bf726e5fc4653b6d0fefa65876d719e5
gagocarrilloedgar <edgargc.upc@gmail.com>
2024-09-23 10:03:50 +0200

    tests: added base conffif for integration and unit testing

--- src/modules/workflows/infra/SupabaseWorkflowsRepository.ts
+++ src/modules/workflows/infra/SupabaseWorkflowsRepository.ts
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 import { FlowWorkflow, WorkflowsRepository } from "@/modules/workflows/domain";
-import { convertKeysToCamelCase } from "./convertKeysToCamelCase";
+import { convertKeysToCamelCase } from "@/shared/lib/convertKeysToCamelCase";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import path from "path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +8,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    include: ["./tests/**/*.test.{tsx,ts}"],
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.ts",
   },
 });
